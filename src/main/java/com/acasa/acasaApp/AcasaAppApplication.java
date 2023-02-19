dc793b1d4191ca91e13593fdf22ce91a63f94f4f
GroupFour77 <sevenseven403@yahoo.com>
2023-02-19 16:02:16 +0800

    first commit

---
+++ src/main/java/com/acasa/acasaApp/AcasaAppApplication.java
@@ -0,0 +1,13 @@
+package com.acasa.acasaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcasaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcasaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acasa/acasaApp/AcasaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acasa.acasaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcasaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
